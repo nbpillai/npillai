695f68baf4356297d2f045697519975b0b8163e7
nbpillai <40529489+nbpillai@users.noreply.github.com>
2018-06-23 16:45:22 -0700

    Add files via upload

---
+++ Odd or Even.py
@@ -0,0 +1,6 @@
+print("We will tell you if this number is odd or even.")
+number = eval(input("Enter a number! "))
+if number%2==0:
+    print("This is an even number!")
+else:
+    print ("This number is an odd number!")
